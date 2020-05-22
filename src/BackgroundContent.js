61fe2cf086cf8dc3504077208f9c3299df3eae74
WeatherApp <alexmflores99@hotmail.ca>
2020-05-22 09:30:27 -0400

    Components added to searchBar class
    Added constructor, a state and method for updating state in searchBar class as well as properties for DropDownBar class. Changed from functions to class to use state.

--- src/BackgroundContent.js
+++ src/BackgroundContent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import defaultImage from './MountainImages/img1.png';
-import navBar from './NavBar.js';
 import NavBar from './NavBar.js';
 function Background(){
 
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,16 +2,45 @@ import React, { Component } from 'react';
 import './App.css';
 import searchIcon from './MountainImages/searchIcon.png';
 import DropdownBar from './DropdownBar.js';
-function SearchBar(){
+
+
+export class SearchBar extends Component{
+    constructor(){ // pass in weather as prop and searchMessage to weatherData. 
+        super();
+        
+        this.state = {
+        searchMessage : "search by city",
+        
+        weather:{
+        
+        temperature : "",
+        city:"",
+        country: ""
+        
+        }
+        }
+    }
+        updateSearchMessage(message){
+
+            this.setState = {
+            
+            searchMessage : message
+            
+            }
+            
+            
+            }
+    
+render(){
 
 return(
 <div>
-<input type = "text" placeholder = " search..." className = "search_box"> 
+<input type = "text" placeholder = " search..." className = "search_box" value = ""> 
 
 </input>
 
 
-<DropdownBar/>
+<DropdownBar updateSearchText= {this.updateSearchMessage}/>
 <div>
 <img src = {searchIcon} className = "shiftSearchIcon"></img>
 </div>
@@ -26,6 +55,6 @@ return(
 
 }
 
-
+}
 
 export default SearchBar;
